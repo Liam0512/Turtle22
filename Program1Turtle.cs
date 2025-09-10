976479affc0bde5f1a4778ee3e9e2ee77b9c4fe8
Liam0512 <cohenrappaportliam@gmail.com>
2025-09-10 13:02:09 +0300

    random points in a circle

--- Program1Turtle.cs
+++ Program1Turtle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,23 +13,60 @@ namespace Turtle22
         /// <summary>
         /// Turtle program
         /// </summary>
+        public static Random rnd = new Random();
         static void Main1()
         {
             Turtle t1 = new Turtle();
 
             //t1.MoveBackward(200);
-            t1.SetDelay(200);
+
+           t1.SetVisible(false);
+            t1.SetDelay(0);
             t1.TailDown();
+            t1.TailUp();
+            t1.TurnRight(60);
+            t1.MoveForward(200);
+            t1.TurnRight(120);
+            t1.MoveForward(200);
+            t1.TurnRight(120);
+            t1.MoveForward(200);
+            t1.TailUp();
+            t1.MoveForward(500);
+            t1.TurnLeft(120);
+            t1.MoveForward(200);
+            t1.TailDown();
+            for (int i = 0; i < 360; i++)
+            {
+                t1.MoveForward(5);
+                t1.TurnLeft(1);
 
-            t1.MoveForward(50);
-            t1.TurnRight(90);
-            t1.MoveForward(100);
+            }
+            t1.MoveForward(286.4766976);
             t1.TurnLeft(90);
-
-            t1.MoveForward(50);
-            t1.TurnRight(90);
-            t1.MoveForward(100);
+            for (int i = 0; i < 3; i++)
+            {
+                t1.MoveForward(572.9577951);
+                t1.TurnLeft(90);
+            }
+            t1.MoveForward(286.4766976);
             t1.TurnLeft(90);
-        } 
+            t1.TailUp();
+            t1.MoveForward(286.4766976);
+            double x;
+            double y;
+            for (int i = 0; i < 500; i++)
+            {
+                x = rnd.NextDouble() * 275;
+                y = rnd.NextDouble() * 360;
+                t1.TurnLeft(y);
+                t1.MoveForward(x);
+                t1.TailDown();
+                t1.MoveForward(5);
+                t1.TailUp();
+                t1.MoveBackward(x + 5);
+                x = rnd.NextDouble() * 300;
+                y = rnd.NextDouble() * 360;
+            }
+        }
     }
 }
