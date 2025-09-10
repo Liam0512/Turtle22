3a709da50674bc4c8e58d0cbf1083da5a2936328
Liam0512 <cohenrappaportliam@gmail.com>
2025-09-10 08:16:46 +0300

    solved the buckets and the frogs

--- Program2Buckets.cs
+++ Program2Buckets.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using Unit4.BucketLib;
 
 namespace Turtle22
@@ -14,28 +13,20 @@ namespace Turtle22
         /// </summary>
         static void Main2()
         {
-            Bucket b = new Bucket(5, "main bucket");
+            Bucket b = new Bucket(20, "main bucket");
+            b.Fill(16);
 
-            Bucket b2 = new Bucket(3, "b2");
+            Bucket b2 = new Bucket(2, "b2");
+            Bucket b7 = new Bucket(7, "b7");
 
-            // b.PourInto(b2);
-            //b2.PourInto(b7);
-            //b7.Empty();
-            //b.PourInto(b7);
+            b.PourInto(b2);
+            b2.PourInto(b7);
+            b7.Empty();
+            b.PourInto(b7);
 
-            //  Console.WriteLine("b : " + b.GetCurrentAmount());
-            // Console.WriteLine("b2: " + b2.GetCurrentAmount());
-            // Console.WriteLine("b7: " + b7.GetCurrentAmount());
-            b2.Fill(3);
-            b2.PourInto(b);
-            b2.Fill(3);
-            b2.PourInto(b);
-            b.Empty();
-            b2.PourInto(b);
-            b2.Fill(3);
-            b2.PourInto(b);
-
-
+            Console.WriteLine("b : " + b.GetCurrentAmount());
+            Console.WriteLine("b2: " + b2.GetCurrentAmount());
+            Console.WriteLine("b7: " + b7.GetCurrentAmount());
         }
     }
 }
--- Program3Frog.cs
+++ Program3Frog.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Unit4.FrogLib;
+﻿using Unit4.FrogLib;
 
 namespace Turtle22
 {
@@ -15,8 +10,8 @@ namespace Turtle22
             BrownFrog f1 = new BrownFrog(5, "f1");
             BrownFrog f2 = new BrownFrog(6, "f2");
 
-            GreenFrog f4 = new GreenFrog(0, "f4"); 
-            GreenFrog f5 = new GreenFrog(1, "f5"); 
+            GreenFrog f4 = new GreenFrog(0, "f4");
+            GreenFrog f5 = new GreenFrog(1, "f5");
             GreenFrog f6 = new GreenFrog(2, "f6");
             f6.Move();
             f0.Jump();
@@ -26,14 +21,7 @@ namespace Turtle22
             f4.Move();
             f0.Move();
             f0.Jump();
-            f1.Jump();
-            f2.Jump();
-            f6.Move();
-            f5.Jump();
-            f4.Jump();
-            f1.Move();
-            f2.Jump();
-            f4.Move();
+
         }
     }
 }
